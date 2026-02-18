e8ef016c1289a76757c04d3097427a064e254650
Damián Bengochea <bengocheadamian@gmail.com>
2026-02-18 16:06:26 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Si querés Cloudflare Pages estático: descomentá
-  // output: "export",
-  // images: { unoptimized: true },
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 export default nextConfig;
